9b60bca07290ce76a0e97e88b814550fc3219265
anthonydieuad-crypto <anthonydieuad@gmail.com>
2025-12-10 18:55:40 +0100

    mise ne place de l'environnement pour la prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production:true,
 // ⚠️ Tu devras remplacer ceci par la VRAIE adresse de ton backend une fois hébergé (ex: Render, Heroku, AWS...)
-    apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'https://coach-assistant-back.onrender.com/api'
 };
