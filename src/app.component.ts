156c210f23db30c91b7d5cce5fddddf342f4d8c1
anthonydieuad-crypto <anthonydieuad@gmail.com>
2025-12-12 14:38:10 +0100

    reprise du responsive + création du menu burger

--- src/app.component.ts
+++ src/app.component.ts
@@ -1,18 +1,26 @@
-import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive
-  ],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        RouterLink,
+        RouterLinkActive
+    ],
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
+    isMobileMenuOpen = signal(false);
 
+    toggleMobileMenu() {
+        this.isMobileMenuOpen.update(v => !v);
+    }
+
+    closeMobilMenu() {
+        this.isMobileMenuOpen.set(false);
+    }
 }
