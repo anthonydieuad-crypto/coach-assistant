dab9071e274bdc1fd55b58a5394006f5d2aca745
anthonydieuad-crypto <anthonydieuad@gmail.com>
2026-04-23 11:03:03 +0200

    limitation des tests à uniquement chromium

--- tests/dashboard.spec.ts
+++ tests/dashboard.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from "@playwright/test";
 
-test.describe('Page Calendrier (Connecté)' , () => {
-    test('doit afficher la page du calendrier directement', async ({page}) => {
+test.describe('Page Dashboard (Connecté)' , () => {
+    test('doit afficher la page du dashboard directement', async ({page}) => {
         await page.goto('/dashboard');
 
         await expect(page).toHaveURL('/dashboard');
