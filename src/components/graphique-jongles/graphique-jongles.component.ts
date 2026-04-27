cb18d829d34c0c864f0611094ee7893917fb8500
anthonydieuad-crypto <anthonydieuad@gmail.com>
2026-04-27 10:03:52 +0200

    deploie force

--- src/components/graphique-jongles/graphique-jongles.component.ts
+++ src/components/graphique-jongles/graphique-jongles.component.ts
@@ -102,4 +102,5 @@ export class GraphiqueJonglesComponent {
     const date = new Date(dateString);
     return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}`;
   }
+  
 }
